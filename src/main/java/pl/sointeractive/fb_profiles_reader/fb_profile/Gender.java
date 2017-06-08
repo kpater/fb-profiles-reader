1cf27cfcebca9ab092e2f7f9f24196825c782092
kpater <kpater87@gmail.com>
2017-06-08 20:34:33 +0200

    Remove commented JsonCreator from Enum Gender.

--- src/main/java/pl/sointeractive/fb_profiles_reader/fb_profile/Gender.java
+++ src/main/java/pl/sointeractive/fb_profiles_reader/fb_profile/Gender.java
@@ -3,21 +3,11 @@ package pl.sointeractive.fb_profiles_reader.fb_profile;
 public enum Gender {
     MALE("male"), FEMALE("female");
 
-    // private static Map<String, Gender> FORMAT_MAP =
-    // Stream.of(Gender.values())
-    // .collect(Collectors.toMap(s -> s.formatted, Function.identity()));
-
     private final String jsonFormat;
 
     Gender(String jsonFormat) {
         this.jsonFormat = jsonFormat;
     }
-
-    // @JsonCreator
-    // public static Gender fromString(String string) {
-    // return Optional.ofNullable(FORMAT_MAP.get(string)).orElseThrow(() -> new
-    // IllegalArgumentException(string));
-    // }
 
     @Override
     public String toString() {
